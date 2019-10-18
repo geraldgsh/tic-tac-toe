054c45de2ddfeccdc86064de4a7bd012d6ca11f7
webmarkyn <web.markyn@gmail.com>
2019-10-18 23:01:45 +0300

    done some of the game logic

--- bin/main.rb
+++ bin/main.rb
@@ -7,20 +7,37 @@ p 'Welcome to the Tic-Tac-Toe game'
 
 board = Board.new
 
+checker = InputManipulator.new
+
 while tic_tac_toe.state
   p "Here's the board"
   board.print_board
-  p 'Enter cell number where you want to place X'
-  user_input = gets.chomp.to_i
-  puts board.input_check(user_input)
+
+  user_input = false
+
+  until checker.state
+    p 'Enter cell number where you want to place X'
+    user_input = gets.chomp.to_i
+    checker.check(user_input, board)
+  end
+
+  user_input = checker.convert_input(user_input)
+
   board.change_cell_cross(user_input)
-  
+
   board.print_board
 
-  p 'Enter cell number where you want to place O'
+  checker.reset
 
-  user_input = gets.chomp
-  puts board.input_check(user_input)
+  until checker.state
+    p 'Enter cell number where you want to place O'
+    user_input = gets.chomp.to_i
+    checker.check(user_input, board)
+  end
+
+  checker.reset
+
+  user_input = checker.convert_input(user_input)
 
   board.change_cell_o(user_input)
 
--- lib/engine.rb
+++ lib/engine.rb
@@ -30,35 +30,65 @@ end
 
 class Board
   def initialize
-    @board_status = {
-      1 => Cell.new,
-      2 => Cell.new,
-      3 => Cell.new,
-      4 => Cell.new,
-      5 => Cell.new,
-      6 => Cell.new,
-      7 => Cell.new,
-      8 => Cell.new,
-      9 => Cell.new
-    }
+    @board_status = Array.new(3) { Array.new(3) { Cell.new } }
   end
 
   def change_cell_o(cell)
-    @board_status[cell].make_o
+    p cell
+    @board_status[cell[0]][cell[1]].make_o
   end
 
   def change_cell_cross(cell)
-    @board_status[cell].make_cross
+    p cell
+    @board_status[cell[0]][cell[1]].make_cross
+  end
+
+  def cell_empty?(cell)
+    p cell
+    p @board_status[cell[0]][cell[1]].state.nil?
+    @board_status[cell[0]][cell[1]].state.nil?
   end
 
   def print_board
-    (1..@board_status.length).each do |i|
-      print "#{@board_status[i].state.nil? ? i : @board_status[i].state} #{(i % 3).zero? && !i.zero? ? "\n" : ''}"
+    # (0..@board_status.length-1).each do |i|
+    #   print "#{@board_status[i].state.nil? ? i : @board_status[i].state} #{(i % 3).zero? && !i.zero? ? "\n" : ''}"
+    # end
+    @board_status.each_with_index do |arr, f_index|
+      arr.each_with_index do |elem, s_index|
+        print(elem.state.nil? ? f_index * 3 + s_index + 1 : elem.state).to_s
+        print " "
+      end
+      puts "\n"
     end
   end
 
-  def input_check(user_input)
-    user_input = user_input.to_i
-    'Input Error' unless (1..9).include?(user_input) && @board_status[user_input].nil? && user_input.zero?
+  def check_board
+    first = 0
+    (1..@board_status.length).each do |i|
+      (i % 3).zero?
+    end
+  end
+end
+
+class InputManipulator
+  attr_reader :state
+
+  def initialize
+    @state = false
+  end
+
+  def convert_input(input)
+    first = (input % 3).zero? ? (input / 3) -1 : (input / 3)
+    second = (input - 1) % 3
+    [first, second]
+  end
+
+  def check(user_input, board)
+    @state = false
+    @state = true if (1..9).include?(user_input) && board.cell_empty?(convert_input(user_input)) && !user_input.zero?
+  end
+
+  def reset
+    @state = false
   end
 end
